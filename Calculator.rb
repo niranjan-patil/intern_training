ec06fcd53115b66976c05c05f05b1ed251a91b38
niranjan <niranjanpatil582@gmail.com>
2019-01-08 16:38:02 +0530

    calculator modified

--- Calculator.rb
+++ Calculator.rb
@@ -1,30 +1,22 @@
-puts "Enter the first number"
-num1 = gets.to_f
-if(!(Float === num1))
-	puts 'Invalid input--Characters not allowed'
-	puts "Enter the first number again"
-	num1 = gets.to_f
+arr = Array.new
+puts 'Enter the numbers:(Enter stop if finished)'
+loop do
+	begin
+		input = gets.chomp
+		break if(input=='stop')
+		num = Integer(input)
+		arr << num
+	rescue
+		puts 'Invalid input'
+	retry
+	end
 end
-num1 = num1.to_f
-
-puts "Enter the second number"
-num2 = gets.to_f
-if(!(Float === num2))
-	puts 'Invalid input--Characters not allowed'
-	puts "Enter the second number again"
-	num2 = gets.to_f
-end
-num2 = num2.to_f
-
-puts "Enter the operation to do (+/-/*/)"
+operation = ['+','-','*','/']
+puts 'Enter the operation'
 oper = gets.chomp
-case oper
-	when "+"
-		puts "Result: #{num1+num2}"
-	when "-"
-		puts "Result: #{num1-num2}"
-	when "*"
-		puts "Result: #{num1*num2}"
-	when "/"
-		puts "Result: #{num1/num2}"
+unless operation.include?(oper)
+	puts 'Invalid operation'
+	puts 'Enter the operation'
+	oper = gets.chomp
 end
+puts arr.inject(oper.to_sym)	
