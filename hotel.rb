f72a10be939d3a90e93bc95c3dd2f3dfe4f4d8e0
niranjan <niranjanpatil582@gmail.com>
2019-01-08 14:45:48 +0530

    hotel.rb modified

--- hotel.rb
+++ hotel.rb
@@ -1,16 +1,11 @@
-Box8 = {"Rajma"=>30, "Vada_Pav"=>50, "Dal"=>20}
-Locavore = {"Dal"=>10, "Vada_Pav"=>30, "Sabji"=>40}
-Flavours = {"Ice_cream"=>10, "Sandwich"=>20, "Burger"=>30, "Vada_Pav"=>30}
-Hotel_list = [Box8,Locavore,Flavours]
+Hotels ={ "Box8" => {"Rajma"=>30, "Vada_Pav"=>50, "Dal"=>20},"Locavore" => {"Dal"=>10, "Vada_Pav"=>30, "Sabji"=>40},
+"Flavours" => {"Ice_cream"=>10, "Sandwich"=>20, "Burger"=>30, "Vada_Pav"=>30}}
 puts "Enter the dishname:"
 dishname = gets.chomp
 puts "Enter the price:"
 dishprice = gets.to_i
-for i in Hotel_list
-	
-	if(!(i[dishname] == nil))
-	 if(i[dishname]<=dishprice)
-	   puts i
-	 end
-	end
+Hotels.each do |key,value|
+		unless Hotels[key][dishname] == nil
+			puts key if(Hotels[key][dishname]<=dishprice)
+		end
 end
